829a30df90a971aec1d1075447fbda88190e3931
Aman sharma <52895888+Aman-Sharmaa@users.noreply.github.com>
2023-07-05 12:45:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -190,4 +190,4 @@ async def predict_audio(audio_file_post: UploadFile = File(...)):
     return {"prediction": predicted_label}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='127.0.0.1', port=10000)
